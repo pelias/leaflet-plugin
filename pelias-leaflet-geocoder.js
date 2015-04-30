f8dc1faf31abe566c0c388a9d4848d09cd62e827
Harish Krishna <harish.krsn@gmail.com>
2015-04-30 10:53:43 -0400

    using toBBoxString

--- pelias-leaflet-geocoder.js
+++ pelias-leaflet-geocoder.js
@@ -32,10 +32,7 @@ L.Control.Geocoder = L.Control.extend({
     } 
 
     var bbox_center  = bbox.getCenter();
-    // TODO: Once https://github.com/pelias/api/pull/117 is resolved,
-    // uncomment the following line :)
-    // params.bbox = bbox.toBBoxString();
-    params.bbox = bbox.getSouth() + ',' + bbox.getWest() + ',' + bbox.getNorth() + ',' + bbox.getEast();
+    params.bbox = bbox.toBBoxString();
     params.lat  = bbox_center.lat;
     params.lon  = bbox_center.lng;
     return params;
